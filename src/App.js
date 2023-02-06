9334ebdc7c585b833b30d02c7b6c2f15720ac0e6
Hoi Wong <jameshf420@gmail.com>
2023-02-06 15:17:12 -0500

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Catalogue from './Components/Catalouge';
-
+import MovieDetails from './Components/MovieDetails';
+import { Routes, Route } from 'react-router-dom';
 const App = () => {
   
   return (
     <div className="wrapper">
       <header>
         <h1>Hackflix</h1>
-        <Catalogue />
       </header>
+    <Routes>
+      <Route path='/' element={<Catalogue />}/>
+
+      <Route path="/movie/:movieID" element={ <MovieDetails /> } />
+    </Routes>
     </div>
   );
 }
